140c3b2f2cbd6e004fcc29228d0ed9a8e4867229
romanfomin <rofominucvt@mail.ru>
2018-02-27 02:08:47 +0300

    first commit

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         CommandLine cmd= SimpleCommandLineParser.parse(args);
         if(cmd==null){
             System.out.println("Wrong cmd arguments");
---
+++ src/main/java/com/epam/interpreter/ControllerReader.java
@@ -0,0 +1,16 @@
+package com.epam.interpreter;
+
+import java.util.concurrent.BlockingQueue;
+
+public class ControllerReader implements Runnable {
+//    private BlockingQueue;
+
+    public ControllerReader(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
