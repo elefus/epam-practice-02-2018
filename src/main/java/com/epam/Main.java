45ba704427051d3a17657d7215d7c8a0e4a4b7bc
romanfomin <rofominucvt@mail.ru>
2018-03-01 12:34:48 +0300

    cmd args fixed

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -4,15 +4,9 @@ import com.epam.interpreter.InterpreterInitializer;
 import com.epam.interpreter.SimpleCommandLineParser;
 import org.apache.commons.cli.CommandLine;
 
-import java.io.ByteArrayInputStream;
-
 public class Main {
 
-
     public static void main(String[] args) {
-
-        ByteArrayInputStream in = new ByteArrayInputStream("!@".getBytes());
-        System.setIn(in);
 
         CommandLine cmd = SimpleCommandLineParser.parse(args);
         if (cmd == null) {
