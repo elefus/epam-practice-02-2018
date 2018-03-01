808ddbd14969af13734af4c58f6c610c890ee431
aleksandrfomin <fsasha_1997@mail.ru>
2018-03-01 12:34:27 +0300

    Optimized Interpreter

--- src/main/java/com/epam/Main.java
+++ src/main/java/com/epam/Main.java
@@ -8,18 +8,10 @@ import java.io.IOException;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
 
-        String inputFile="./src/test/resources/tests/summation.bf";
-        String outputFile="./src/test/resources/tests/computedResults/summation_result.bf";
-        String answerFile="./src/test/resources/tests/answers/summation_answer.bf";
-        String[] strs = {"-source", inputFile,
-                "-out", outputFile,
-                "-optimize"};
         CommandLine cmd;
-        cmd = ArgsParser.parseArgs(strs);
+        cmd = ArgsParser.parseArgs(args);
         try {
             new OptimizedInterpreterInitializer(cmd);
         } catch (IOException e) {
